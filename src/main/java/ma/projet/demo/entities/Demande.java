950b68a6d49e7a56192f6a44c08a71677da50942
reda108 <redabouougri12@gmail.com>
2023-12-27 21:34:04 +0100

    fix

--- src/main/java/ma/projet/demo/entities/Demande.java
+++ src/main/java/ma/projet/demo/entities/Demande.java
@@ -1,8 +1,10 @@
 package ma.projet.demo.entities;
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
